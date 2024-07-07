9d22fce5d57167aa4c0c185189bd4b5dfc2db66d
am33rr <am33rrss@gmail.com>
2024-07-07 15:26:17 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,12 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
 
-  const token = await hre.ethers.deployContract("MetaToken");
+  const alienContract = await hre.ethers.getContractFactory("Alien");
+  const ahc=await alienContract.deploy();
+  await ahc.deployed();
 
-  console.log("Token address:", await token.getAddress());
+  console.log("Alien Ruling Humans Contract Address:", ahc.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
